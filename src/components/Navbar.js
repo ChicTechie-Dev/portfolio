40197269f82560be09f0a1693e7d1837b0a73606
tvakash <akash.verma@techvalens.com>
2025-01-13 12:55:08 +0530

    added github url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -107,7 +107,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/ChicTechie-Dev"
                 target="_blank"
                 className="fork-btn-inner"
               >
